4c7bccb2f6121abbcaebdab02eaad6e8ca433518
sopheakreth <rethsopheak@gmail.com>
2016-06-12 22:42:39 +0700

    update all

---
+++ js/js.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	// $('#search').on('click', function(){
+
+	// 		$('#formsearch').slideDown('slow').css({
+
+	// 			'position': 'absolute',
+	// 			'display' : 'block',
+	// 			'right' : 10,
+	// 			'top'	: 10
+
+	// 		});
+	// });
+
+});
